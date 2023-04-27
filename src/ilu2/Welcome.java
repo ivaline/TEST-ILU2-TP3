6546b7cc22bd7a792ffd260b88517b72700d137b
ivaline <alina.iva.4@gmail.com>
2023-04-27 16:04:16 +0200

    TP fini

--- src/ilu2/Welcome.java
+++ src/ilu2/Welcome.java
@@ -7,6 +7,59 @@ import java.util.Objects;
 
 public class Welcome {
 	
+	//pour l'exigence 9
+	public static void tableOfNames(String input, List<String> tableauNames,List<Integer> countingNames) {
+		
+		for (String name1 : input.split(",")) {
+			 name1 = name1.trim();
+			 if (tableauNames.contains(name1)) {
+				 int i = 0;
+				 for(String aName: tableauNames) {
+					 if (Objects.equals(name1, aName)) {
+						 countingNames.set(i,countingNames.get(i)+1);
+						}
+					 i++;
+				 }
+			 }
+			 else {
+				 tableauNames.add(name1.trim());
+				 countingNames.add(1);
+			 }
+		 }
+	}
+	
+	//pour les exigences a partir de EX_3
+	public static void upperCaseString(StringBuilder nameBig, int motsBig, String dernMotBig) {
+		 if (motsBig>1) {
+			 int index = nameBig.indexOf(dernMotBig);
+			 nameBig.replace(index-2, nameBig.length(), " AND " + dernMotBig);
+			 //enlever le "," apres HELLO si on a plus que 1 nom
+			 nameBig.delete(11, 12); 
+		 }
+		 int indexYODA = nameBig.indexOf("YODA");
+		 if(indexYODA!=-1) {
+			 nameBig.delete(5,11);
+			 nameBig.append(" HELLO");
+			 }
+		 nameBig.append(" !");
+	}
+	
+	//pour les exigences a partir de EX_1
+	public static void lowerCaseString(StringBuilder nameSmall, int motsSmall, String dernMotSmall) {
+		if (motsSmall>1) {
+			 int index = nameSmall.indexOf(dernMotSmall);
+			 nameSmall.replace(index-2, nameSmall.length(), " and " + dernMotSmall);
+		 }
+	 
+		int indexYoda = nameSmall.indexOf("Yoda");
+		if(indexYoda!=-1) {
+			nameSmall.delete(0,7);
+			nameSmall.append(", Hello");
+	 	}
+	}
+	
+	
+	
 	public static String welcome(String input) {
 		
 			 if (input.trim() == "")
@@ -17,34 +70,24 @@ public class Welcome {
 			 } 
 			 
 			 
+			 
+			 List<String> tableauNames = new ArrayList<>();
+			 List<Integer> countingNames = new ArrayList<>();
+			 tableOfNames(input, tableauNames,countingNames);
+					
+			 
 			 StringBuilder nameSmall = new StringBuilder();
 			 StringBuilder nameBig = new StringBuilder();
 			 nameSmall.append("Hello");
 			 nameBig.append(". AND HELLO");
-			 String dernMotSmall = null;//String pour le Hello jusqu'a AND HELLO
-			 int motsSmall = 0;//combien des noms sont pour le Hello
-			 int motsBig = 0;//combien des noms sont pour le HELLO
-			 String dernMotBig = null;// String a partir de AND HELLO
-			 List<String> tableauNames = new ArrayList<>();
-			 List<Integer> countingNames = new ArrayList<>();
-			 for (String name1 : input.split(",")) {
-				 name1 = name1.trim();
-				 if (tableauNames.contains(name1)) {
-					 int i = 0;
-					 for(String aName: tableauNames) {
-						 if (Objects.equals(name1, aName)) {
-							 countingNames.set(i,countingNames.get(i)+1);
-							}
-						 i++;
-					 }
-				 }
-				 else {
-					 tableauNames.add(name1.trim());
-					 countingNames.add(1);
-				 }
-			 }
-			int iName = 0;		 
-			for (String name2: tableauNames) {
+			 
+			 String dernMotSmall = null, dernMotBig = null;
+			 int motsSmall = 0, motsBig = 0;//combien des noms sont pour "Hello" et "HELLO"
+			 
+			 int iName = 0;
+			 
+			 //pour l'exigence 9 ou on compte les noms
+			 for (String name2: tableauNames) {
 				 if(name2.toUpperCase() == name2) {
 					 if (countingNames.get(iName)>1) {
 						 name2 = name2+"(x" + countingNames.get(iName)+")";
@@ -52,7 +95,7 @@ public class Welcome {
 					 nameBig.append(", "+name2); 
 					 motsBig ++;
 					 dernMotBig = name2;
-					 }
+				 }	
 				else {
 					name2 = name2.substring(0, 1).toUpperCase()+name2.substring(1);
 					if (countingNames.get(iName)>1) {
@@ -61,9 +104,9 @@ public class Welcome {
 					nameSmall.append(", "+name2);
 					motsSmall++;
 					dernMotSmall = name2;
-						 }
+				}
 				 iName++;
-					 }
+			}
 		
 			 /*j'ai fait cette ligne de code parce que dans les premieres test il 
 			 ne faudrait avoir "AND HELLO"*/
@@ -71,29 +114,17 @@ public class Welcome {
 				 nameBig.delete(0, nameBig.length());
 			 }
 			 else {
-				 if (motsBig>1) {
-					 int index = nameBig.indexOf(dernMotBig);
-					 nameBig.replace(index-2, nameBig.length(), " AND " + dernMotBig);
-					 //enlever le "," apres HELLO si on a plus que 1 nom
-					 nameBig.delete(11, 12); 
-				 }
-				 int indexYODA = nameBig.indexOf("YODA");
-				 if(indexYODA!=-1) {
-					 nameBig.delete(5,11);
-					 nameBig.append(" HELLO");
-					 }
-				 nameBig.append(" !");
+				 upperCaseString(nameBig,motsBig, dernMotBig);
 			 }
 			 
-			 if (motsSmall>1) {
-				 int index = nameSmall.indexOf(dernMotSmall);
-				 nameSmall.replace(index-2, nameSmall.length(), " and " + dernMotSmall);
-			 }	 
-			 int indexYoda = nameSmall.indexOf("Yoda");
-			 if(indexYoda!=-1) {
-				 nameSmall.delete(0,7);
-				 nameSmall.append(", Hello");
+			 
+			 if (motsSmall==0) {
+				 nameSmall.delete(0,nameSmall.length());
 			 }
+			 else {
+				 lowerCaseString(nameSmall,motsSmall,dernMotSmall);
+			 }
+			 
 			 
 			 return nameSmall.toString() + nameBig.toString();
 	}
